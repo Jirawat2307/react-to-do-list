bba78dcd9c432ba5486a3f35073d0f4d7f3e426e
Jirawat Phattarawasitpol <jirawat.phattarawasitpol@gmail.com>
2024-03-26 15:34:57 +0700

    fetch todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { BASE_API } from "./constants/base";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    interface todaType {
+        id: number;
+        todo: string;
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    const [todos, setTodos] = useState<todaType[]>([]);
 
-export default App
+    const getTodos = async () => {
+        const response = await fetch(`${BASE_API}/todos`, {
+            method: "GET",
+        });
+
+        let todos = await response.json();
+
+        setTodos(todos);
+    };
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+        <div className="todoBlock">
+            <h1>Todo list</h1>
+            {todos.map((todo: todaType) => (
+                <div key={todo.id} className="todo">
+                    <h3>{todo.todo}</h3>
+                    <button>Edit</button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default App;
---
+++ src/constants/base.ts
@@ -0,0 +1,1 @@
+export const BASE_API = "https://66027a8b9d7276a7555351ce.mockapi.io";
